7c19449f6c0e57a56ab8d90c185db39a60b55899
tdietert <tdietert@mail.usf.edu>
2017-09-05 17:22:24 -0400

    More work on datetime calendar arithmetic

--- src/Types.hs
+++ src/Types.hs
@@ -51,17 +51,23 @@ instance Serialize Datetime where
 -- Deltas and Intervals
 -------------------------------------------------------------------------------
 
+-- | A time difference represented with y/m/d + h/m/s/ns
 data Delta = Delta
-  { dPeriod   :: Period
-  , dDuration :: Duration
+  { dPeriod   :: Period   -- ^ An amount of conceptual calendar time in terms of years, months and days.
+  , dDuration :: Duration -- ^ An amount of time measured in hours/mins/secs/nsecs
   }
 
+-- | A time period between two Datetimes
 data Interval = Interval
-  { istart :: Datetime
+  { iStart :: Datetime
   , iStop  :: Datetime
   }
 
-
+-- | Conversion function between Data.Hourglass.DateTime and Datetime defined in
+-- this module.
+--
+-- This should be the only way to construct a Datetime value, given the use of
+-- the partial toEnum function in the Datetime -> DateTime conversion functions
 convert :: DateTime -> Datetime
 convert dt = Datetime {
     year     = dateYear (dtDate dt)
@@ -73,6 +79,26 @@ convert dt = Datetime {
   , zone     = 0
   , week_day = fromEnum $ getWeekDay (dtDate dt)
   }
+
+-- | Conversion function between Datetime and Data.Hourglass.DateTime
+convert' :: Datetime -> DateTime
+convert' dt = DateTime {
+      dtDate = dtDate'
+    , dtTime = dtTime'
+    }
+  where
+    dtDate' = Date {
+        dateYear  = year dt
+      , dateMonth = toEnum (month dt)
+      , dateDay   = day dt
+      }
+
+    dtTime' = TimeOfDay {
+        todHour = fromIntegral (hour dt)
+      , todMin  = fromIntegral (minute dt)
+      , todSec  = fromIntegral (second dt)
+      , todNSec = 0
+      }
 
 days :: Int -> Period
 days n = Period { periodYears = 0, periodMonths = 0, periodDays = n }
@@ -93,29 +119,59 @@ fortnights n = weeks (2*n)
 -- Calendar Arithmetic
 -------------------------------------------------------------------------------
 
--- | Ad a period to a date
-add :: Date -> Period -> Date
-add = undefined  -- XXX
+-- | Add a delta to a date
+add :: Datetime -> Delta -> Datetime
+add dt (Delta period duration) =
+    convert $ DateTime (dateAddPeriod d period) tod
+  where
+    -- Data.Hourglass.DateTime with duration added
+    dt'@(DateTime d tod) = timeAdd (convert' dt) duration
 
--- | Subtract a period from a date
-sub :: Date -> Delta -> Date
-sub = undefined  -- XXX
+-- | Subtract a delta from a date (Delta should be positive)
+sub :: Datetime -> Delta -> Datetime
+sub dt (Delta period duration) =
+  add dt $ Delta (negatePeriod period) (negateDuration duration)
 
 -- | Get the difference between two dates
-diff :: Date -> Date -> Delta
-diff = undefined -- XXX
+diff :: Datetime -> Datetime -> Delta
+diff d1 d2 = undefined
+  where
+    diffSecs = timeDiff (convert' d1) (convert' d2)
 
 -- | Check whether a date lies within an interval
-within :: Date -> Interval -> Bool
-within = undefined -- XXX
+within :: Datetime -> Interval -> Bool
+within dt (Interval start stop) =
+    startDate <= origDate && origDate <= endDate
+  where
+    origDate  = convert' dt
+    startDate = convert' start
+    endDate   = convert' stop
 
 -- | Get the difference (in days) between two dates
-daysBetween :: Date -> Date -> Delta
-daysBetween = undefined -- XXX
+daysBetween :: Datetime -> Datetime -> Delta
+daysBetween d1'' d2'' =
+    Delta (Period 0 0 durDays) mempty
+  where
+    d1' = convert' d1''
+    d2' = convert' d2''
 
--- | Get the date of the last day in a month
-eomonth :: Month -> Date
-eomonth = undefined -- XXX
+    (d1,d2)  = if d1' <= d2' then (d1',d2') else (d2',d1')
+    duration = fst $ fromSeconds $ timeDiff d1 d2
+    durDays  = let (Hours hrs) = durationHours duration in fromIntegral hrs `div` 24
+
+-- | Get the date of the first day in a month of a given year
+fomonth :: Int -> Month -> Datetime
+fomonth y m = convert $ DateTime (Date y m 1) (TimeOfDay 0 0 0 0)
+
+-- | Get the date of the last day in a month of a given year
+eomonth :: Int -> Month -> Datetime
+eomonth y m = sub foNextMonth $ Delta (Period 0 0 1) mempty
+  where
+    nextMonth
+      | fromEnum m == 11 = January
+      | otherwise = toEnum $ fromEnum m + 1
+
+    foNextMonth = convert $ DateTime (Date y nextMonth 1) (TimeOfDay 0 0 0 0)
 
 -------------------------------------------------------------------------------
 -- Queries
@@ -137,3 +193,13 @@ isWeekend = not . isWeekday
 
 isBusiness :: Date -> Bool
 isBusiness dt = not (isHoliday dt) && not (isWeekend dt)
+
+-------------------------------------------------------------------------------
+-- Helpers
+-------------------------------------------------------------------------------
+
+negatePeriod :: Period -> Period
+negatePeriod (Period y m d) = Period (-y) (-m) (-d)
+
+negateDuration :: Duration -> Duration
+negateDuration (Duration h m s ns) = Duration (-h) (-m) (-s) (-ns)
